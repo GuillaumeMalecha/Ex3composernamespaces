9245453108dee5ffdfae97cebe4f42870f3bc2b3
GuillaumeMalecha <guillaume.malecha@gmail.com>
2022-10-14 17:07:13 +0200

    Update PersonneManager.php

--- src/manager/PersonneManager.php
+++ src/manager/PersonneManager.php
@@ -7,5 +7,8 @@ class PersonneManager extends Personne
     public function Create()
     {
         $faker = Faker\Factory::create();
+        $faker->name;
+        $faker->address;
+        $faker->phoneNumber;
     }
 }
